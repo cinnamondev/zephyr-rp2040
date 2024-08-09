e908d916c59dc7ab0212cdcb7b96ae4d4f0609ee
cinnamon <justpromptiguess@gmail.com>
2024-08-09 18:54:15 +0100

    working demo slkow

--- src/main.c
+++ src/main.c
@@ -20,6 +20,7 @@ LOG_MODULE_REGISTER(app, LOG_LEVEL_DBG);
 
 int main(void)
 {
+	LOG_DBG("test");
 	const struct device *display_dev;
 	lv_obj_t *hello_world_label;
 
@@ -28,15 +29,17 @@ int main(void)
 		LOG_ERR("Device not ready, aborting test");
 		return 0;
 	}
+	//	LOG_DBG("test");
 	//hello_world_label = lv_label_create(lv_scr_act());
 	//lv_label_set_text(hello_world_label, "Hello world!");
 	//lv_obj_align(hello_world_label, LV_ALIGN_CENTER, 0, 0);
 	lv_demo_music();
+	LOG_DBG("test");
 	lv_task_handler();
 	display_blanking_off(display_dev);
+		LOG_DBG("test");
 
 	while (1) {
-		lv_task_handler();
-		k_sleep(K_MSEC(10));
+		k_msleep(lv_task_handler());
 	}
 }
